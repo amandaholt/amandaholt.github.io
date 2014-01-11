09151d9e25b68960fc9248052fed8da0d834fe46
Amanda Holt <amanda@tradecrafted.com>
2014-01-10 17:50:35 -0800

    added javascript

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,7 +1,10 @@
 // Your Javascript will go here!
+
   function replaceName() {
     var myNewName = window.prompt('Pick a new name!');
     document.getElementById('user-name').innerHTML = myNewName;
-	}
+  }
 
   document.onclick = replaceName;
+
+
