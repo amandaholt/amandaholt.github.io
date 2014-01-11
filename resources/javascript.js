de5a691225fb506a6d51f1e2f3f2c095027cf07c
Amanda Holt <amanda@tradecrafted.com>
2014-01-10 17:55:04 -0800

    added copyright javascript

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,5 +1,13 @@
 // Your Javascript will go here!
 
+$(document).ready(function() {
+  	$("#copyright").click(function(){ 
+  		$(this).css('color','purple'); 
+	});
+});
+
+
+
   function replaceName() {
     var myNewName = window.prompt('Pick a new name!');
     document.getElementById('user-name').innerHTML = myNewName;
